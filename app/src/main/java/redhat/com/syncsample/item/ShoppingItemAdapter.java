df945c9b7319bd7fbda100df2054d5e4d844b3ed
Summers Pittman <secondsun@gmail.com>
2015-10-14 11:12:52 -0400

    AutoLicense

--- app/src/main/java/redhat/com/syncsample/item/ShoppingItemAdapter.java
+++ app/src/main/java/redhat/com/syncsample/item/ShoppingItemAdapter.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2015 FeedHenry Ltd, All Rights Reserved.
- * <p>
+ *
  * Please refer to your contract with FeedHenry for the software license agreement.
  * If you do not have a contract, you do not have a license to use this software.
  */
